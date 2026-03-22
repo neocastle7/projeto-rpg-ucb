67ef374111b0d547a0eea347687a34346c4eb70d
Jackson <jacksonsantosdasilvab@gmail.com>
2026-03-22 12:35:51 -0300

    calculos.java corrigido

--- src/br/ucb/rpg/logic/Calculos.java
+++ src/br/ucb/rpg/logic/Calculos.java
@@ -9,16 +9,23 @@ public class Calculos {
     private static Random random = new Random();
 
     // Metodo responsável por calcular o dano e retorna um valor inteiro
-    public static int calcDano(int ataqueBase){
+    public static int calcDano(int ataqueBase, int variacao){
+        //ataqueBase = valor do ataque 10
+        //ex  de variacao = 2
 
         // Define o valor minimo do dano
-        int minimo = ataqueBase - 2;
+        //garante que o dano minimo nao seja negativo
+        //ataqueBase = valor do ataque 10
+        //ex  de variacao = 2
+        //minimo = 10- 2 (8)
+        int minimo = Math.max(0, ataqueBase - variacao);
 
         // Define o valor maximo do dano
-        int maximo = ataqueBase + 2;
+
+        int maximo = ataqueBase + variacao;
 
         // (maximo - minimo + 1) define o tamanho do intervalo
-        //ex: ataquegit add . (Prepara os arquivos).(10) = (12 - 8 + 1) = 5
+        //ex: ataque(10) = (12 - 8 + 1) = 5
         //nextInt() - gera numero de 0 ate limite-1 (5-1)
         // Soma o valor mínimo para que o número aleatório comece a partir dele
         // EX: 0 + 8 = 8, comeca apartir de 8 e vai ate (4+min) 12
