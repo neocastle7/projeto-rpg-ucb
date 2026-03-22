6f4640c7002fcf1734ed840a43580780bebba851
Jackson <jacksonsantosdasilvab@gmail.com>
2026-03-22 12:11:19 -0300

    Salvando class Calculos

--- src/br/ucb/rpg/logic/Calculos.java
+++ src/br/ucb/rpg/logic/Calculos.java
@@ -1,8 +1,33 @@
 package br.ucb.rpg.logic;
 
+import java.util.Random;
+//importa a classe Random, para gerar numeros aleatorios
+
 public class Calculos {
-    // Jackson:
-    // 1. Pesquise como usar a classe 'java.util.Random'. [cite: 43]
-    // 2. Crie um método que retorne um número aleatório para ser usado no dano do combate.
-    // 3. Isso vai garantir que os ataques não tirem sempre o mesmo valor. [cite: 43]
+
+    // Cria um gerador de números aleatórios
+    private static Random random = new Random();
+
+    // Metodo responsável por calcular o dano e retorna um valor inteiro
+    public static int calcDano(int ataqueBase){
+
+        // Define o valor minimo do dano
+        int minimo = ataqueBase - 2;
+
+        // Define o valor maximo do dano
+        int maximo = ataqueBase + 2;
+
+        // (maximo - minimo + 1) define o tamanho do intervalo
+        //ex: ataquegit add . (Prepara os arquivos).(10) = (12 - 8 + 1) = 5
+        //nextInt() - gera numero de 0 ate limite-1 (5-1)
+        // Soma o valor mínimo para que o número aleatório comece a partir dele
+        // EX: 0 + 8 = 8, comeca apartir de 8 e vai ate (4+min) 12
+        //ele escolhe um valor aleatorio entre "8 e 12"
+
+        int dano = random.nextInt(maximo - minimo + 1) + minimo;
+
+        // Retorna o valor final do dano
+        return dano;
+    }
+
 }
